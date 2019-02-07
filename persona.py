a8164f0fdde6e8ca894ee6a492a1f44d3ed2ea13
Maria Rolon <malurolon@gmail.com>
2019-02-07 11:37:26 -0300

    se realizaron cambios

--- persona.py
+++ persona.py
@@ -7,13 +7,25 @@
 #atributos
 
 class Persona:
-    def __init__ (self, un_nombre="",una_edad="",una_profesion=""):
+    def __init__ (self, un_nombre="",una_edad= 0,una_profesion=""): #aqui se carga una edad vacia en 
+#numero para ir asignando
         self.nombre= un_nombre
         self.edad= una_edad
         self.profesion= una_profesion
         print("Hola me llamo",self.nombre,"tengo", self.edad, "y soy", self.profesion)
+    
+    def cumpleanhos (self):
+        self.edad= self.edad +1 #este metodo suma 1 a la edad cada vez que se le llama
+        return self.edad 
 
-humano= Persona ("Maria", "33", "profesor")
+humano= Persona ("Maria", 33, "profesor")
+print (humano.cumpleanhos())
+print(humano.cumpleanhos())
+
 print(humano.nombre)
 print(humano.edad)
 print(humano.profesion)
+
+#Agregar un metodo a la clase persona que se llame cumpleanhos y que aumente la edad de la persona
+# en un anho y retorne la edad nuevA
+
